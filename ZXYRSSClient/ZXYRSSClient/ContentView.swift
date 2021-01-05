90caad7e58cbf090ef83817faadfd4878d7c0626
zhaoxiangyu <zhaoxiangyu@kuaishou.com>
2021-01-05 17:38:48 +0800

    add project

---
+++ ZXYRSSClient/ZXYRSSClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZXYRSSClient
+//
+//  Created by zhaoxiangyu on 2021/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZXYRSSClient/ZXYRSSClient/ZXYRSSClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZXYRSSClientApp.swift
+//  ZXYRSSClient
+//
+//  Created by zhaoxiangyu on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct ZXYRSSClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
